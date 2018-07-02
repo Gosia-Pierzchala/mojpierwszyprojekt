69866275fc64e2047ea40544a825442886176ed8
Małgorzata Pierzchała <gk.kalinowska@gmail.com>
2018-07-02 18:59:30 +0200

    Dodanie Super!

--- src/HellowWorld.java
+++ src/HellowWorld.java
@@ -2,7 +2,6 @@ public class HellowWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello");
-//        System.out.println("Hello");
-//        System.out.println("Hello");
+        System.out.println("Super!");
     }
 }
